425370623f6b8dac2b7145a9b62caa991446b3a3
TheMasterDodo <william.wu1219@gmail.com>
2017-01-29 21:40:52 -0500

    Update mybotworks.js

--- mybotworks.js
+++ mybotworks.js
@@ -2,9 +2,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("/Users/Shared/config.json");
 const setTable = require(config.DataFilePath + "/FWTSetData.json");
-const aliasList = require(config.DataFilePath + "/FWTSetAliases.json");
+const aliasListSets = require(config.DataFilePath + "/FWTSetAliases.json");
+const aliasListHeroes = require(config.DataFilePath + "/FWTHeroAliases.json");
 const rainbowRotation = require(config.DataFilePath + "/FWTSetRotation.json");
-const HeroDataTable = require(config.DataFilePath + "/FWTHeroStats.json");
+const heroDataTable = require(config.DataFilePath + "/FWTHeroStats.json");
 
 for (let i = 0, len = setTable.length; i < len; i++) {
     for (let j = 0, weeks = rainbowRotation.length; j < weeks; j++) {
@@ -13,6 +14,16 @@ for (let i = 0, len = setTable.length; i < len; i++) {
             setTable[i]["Last Time in the Rotation"] = rainbowRotation[j]["Week"];
         }
     }
+}
+
+function createOutput(list) {
+    var dataString = "";
+    for (var property in list) {
+        if (list.hasOwnProperty(property)) {
+            dataString = dataString + property + ": " + list[property] + "\n";
+        }
+    }
+    return dataString;
 }
 
 function coocooPull(isLast) {
@@ -48,70 +59,50 @@ function findEmojiFromGuildByName(guild, emoji_name) {
     return emoji ? emoji.toString() : emoji_name;
 }
 
-function SetNameByAlias(SetAlias) {
+function findNameByAlias(alias, isSet) {
+    if (isSet) var aliasList = aliasListSets;
+    else var aliasList = aliasListHeroes;
     for (var i = 0, setnum = aliasList.length; i < setnum; i++) {
         for (var j = 0, len = aliasList[i]["aliases"].length; j < len; j++) {
-          	if (aliasList[i]["aliases"][j] == SetAlias) return aliasList[i]["name"];
+          	if (aliasList[i]["aliases"][j] == alias) return aliasList[i]["name"];
         }
     }
   	return "nosuchalias";
 }
 
-function findSet(SetAlias) {
-  	var name = SetNameByAlias(SetAlias);
+function findSet(setAlias) {
+  	var name = findNameByAlias(setAlias, true);
   	if (name == "nosuchalias") return "nosuchset";
     var setData = setTable[0];
-
     for (var i = 1, len = setTable.length; i < len; i++) {
         if (setTable[i]["Name"] == name) setData = setTable[i];
     }
 
-    var dataString = "";
-    for (var property in setData) {
-        if (setData.hasOwnProperty(property)) {
-            dataString = dataString + property + ": " + setData[property] + "\n";
-        }
-    }
-
-    return dataString;
+    return createOutput(setData);
 }
 
 function PullOrNot() {
     var number = Math.random();
     var YesNo;
-    if (number <= 0.5) YesNo =  config.DataFilePath + "/Images/Pull.png";
-    else YesNo = config.DataFilePath + "/Images/Don't Pull.png";
+    if (number <= 0.5) YesNo =  config.FilePath + "/Images/Pull.png";
+    else YesNo = config.FilePath + "/Images/Don't Pull.png";
     return YesNo;
 }
 
 function SetsOfTheWeek(WeekRequested) {
-    var RotationLength = rainbowRotation.length;
-    var rainbowData = rainbowRotation[RotationLength - 1 - WeekRequested];
-    var dataString = "";
-
-    for (var property in rainbowData) {
-        if (rainbowData.hasOwnProperty(property)) {
-            dataString = dataString + property + ": " + rainbowData[property] + "\n";
-        }
-    }
-    return dataString;
+    var rainbowData = rainbowRotation[rainbowRotation.length - 1 - WeekRequested];
+    return createOutput(rainbowData);
 }
 
-function HeroData(HeroRequested) {
+function findHeroData(alias) {
+    var name = findNameByAlias(alias, false);
+    if (name == "nosuchalias") return "nosuchhero";
+    var heroData = heroDataTable[0];
 
-    var HeroData = HeroDataTable[0];
-
-    for (var i = 1, len = HeroDataTable.length; i < len; i++) {
-        if (HeroDataTable[i]["Name"] == HeroRequested) HeroData = HeroDataTable[i];
+    for (var i = 1, len = heroDataTable.length; i < len; i++) {
+        if (heroDataTable[i]["Name"] == name) heroData = heroDataTable[i];
     }
-    var dataString = "";
-
-    for (var property in HeroData) {
-        if (HeroData.hasOwnProperty(property)) {
-            dataString = dataString + property + ": " + HeroData[property] + "\n";
-        }
-    }
-    return dataString;
+    return createOutput(heroData);
 }
 
 bot.on("message", msg => {
@@ -124,62 +115,42 @@ bot.on("message", msg => {
     }
 
 
-    if (msg.content.startsWith(config.prefix + "ping")) { // Testing purposes
-        msg.channel.sendMessage("pong!");
+    if (msg.content.startsWith(config.prefix + "ping")) msg.channel.sendMessage("pong!"); // Testing purposes
 
-    } else if (msg.content.startsWith(config.prefix + "tadaima") && (msg.content.includes("maid"))) {
-        msg.channel.sendMessage("おかえりなさいませ！ご主人様♥, \nDo you want dinner or a shower or \*blushes\* me?");
-
-    } else if (msg.content.startsWith(config.prefix + "tadaima") && (msg.content.includes("spades"))) {
-        msg.channel.sendMessage("おかえりなさいませ！ご主人様 :anger:, \nWell, I don't have much of a choice. I guess I'll end this here since I got ~~Shido~~ Spades-san to pat my head today.----right, all of me?");
-
-    } else if (msg.content.startsWith(config.prefix + "tadaima")) {
-        msg.channel.sendMessage("Okaeri dear, \nDo you want dinner or a shower or \*blushes\* me?");
-
-    } else if (msg.content.startsWith(config.prefix + "tuturu")) { // Tuturu
-        msg.channel.sendFile(config.FilePath + "/Images/Tuturu.png"); 
     
-    } else if (msg.content.startsWith(config.prefix + "pull")) { // Single pull
-        const ShouldIPull = PullOrNot();
-        msg.channel.sendFile(ShouldIPull);
-
-    } else if (msg.content.startsWith(config.prefix + "whale")) { // 10x pull
+    else if (msg.content.startsWith(config.prefix + "tadaima") && (msg.content.includes("maid"))) msg.channel.sendMessage("おかえりなさいませ！ご主人様♥, \nDo you want dinner or a shower or \*blushes\* me?");
+    else if (msg.content.startsWith(config.prefix + "tadaima") && (msg.content.includes("spades"))) msg.channel.sendMessage("おかえりなさいませ！ご主人様 :anger:, \nWell, I don't have much of a choice. I guess I'll end this here since I got ~~Shido~~ Spades-san to pat my head today.----right, all of me?");
+    else if (msg.content.startsWith(config.prefix + "tadaima")) msg.channel.sendMessage("Okaeri dear, \nDo you want dinner or a shower or \*blushes\* me?");
+    else if (msg.content.startsWith(config.prefix + "tuturu")) msg.channel.sendFile(config.FilePath + "/Images/Tuturu.png"); // Tuturu
+    else if (msg.content.startsWith(config.prefix + "moe")) msg.channel.sendFile(config.FilePath + "/Images/Shushu/moe.PNG");
+    //end of random irrelevant stuff
+    
+    else if (msg.content.startsWith(config.prefix + "pull")) msg.channel.sendFile(PullOrNot());
+    
+    else if (msg.content.startsWith(config.prefix + "whale")) { // 10x pull
         const pulls = coocooPull10().map((emoji_name) => findEmojiFromGuildByName(msg.guild, emoji_name));
         msg.channel.sendMessage(pulls.join(" "));
 
     } else if (msg.content.startsWith(config.prefix + "set")) { // Searches database for set info
-        var message = msg.content;
-        var messageLength = message.length;
-        var setLocation = message.indexOf(" ", 0);
-        var setName = message.slice(setLocation + 1, messageLength);
+        var setName = msg.content.slice(msg.content.indexOf(" ", 0) + 1, msg.content.length);
         var setInfo = findSet(setName.toLowerCase());
         if (setInfo != "nosuchset") msg.channel.sendMessage(setInfo);
         else msg.channel.sendMessage("Unknown Set!");
 
-    } else if (msg.content.startsWith(config.prefix + "moe")) {
-        msg.channel.sendFile(config.FilePath + "/Images/moe.PNG");
-
     } else if (msg.content.startsWith(config.prefix + "nameset") && (msg.author.id == config.ownerID)) {
-        const member = msg.guild.member(bot.user);
-        member.setNickname("A Certain Magical Bot");
+        msg.guild.member(bot.user).setNickname("A Certain Magical Bot");
         msg.channel.sendMessage("My name has been set!");
 
     } else if (msg.content.startsWith(config.prefix + "rainbow")) {
-        var message = msg.content;
-        var messageLength = message.length;
-        var weekLocation = message.indexOf(" ",0);
-        if (weekLocation != -1) {
-            var WeekRequested = message.slice(weekLocation + 1, messageLength);
+        if (msg.content.indexOf(" ",0) != -1) {
+            var WeekRequested = msg.content.slice(msg.content.indexOf(" ",0) + 1, msg.content.length);
         } else WeekRequested = 0;
         const currentSets = SetsOfTheWeek(WeekRequested);
         msg.channel.sendMessage(currentSets);
 
     } else if (msg.content.startsWith(config.prefix + "stats")) {
-        var message = msg.content;
-        var messageLength = message.length;
-        var HeroLocation = message.indexOf(" ",0);
-        var HeroRequested = message.slice(HeroLocation + 1, messageLength);
-        var HeroStats = HeroData(HeroRequested);
+        var HeroRequested = msg.content.slice(msg.content.indexOf(" ", 0) + 1, msg.content.length);
+        var HeroStats = findHeroData(HeroRequested);
         msg.channel.sendMessage(HeroStats);
     } 
 });
